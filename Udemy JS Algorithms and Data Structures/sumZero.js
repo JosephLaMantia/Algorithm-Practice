247260146cfc6c7b185c098bfb7ce292e8e371c9
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-02-26 14:18:39 -0500

    outlined with pseudocode

--- Udemy JS Algorithms and Data Structures/sumZero.js
+++ Udemy JS Algorithms and Data Structures/sumZero.js
@@ -3,5 +3,11 @@
 //returns that pair, or undefined if DNE (no return)
 
 function sumZero (arr) {
-    
+    //create pointers for the left and rightmost elements of arr
+    //while the left is smaller than right
+        //create sum variable = left + right
+        //check if sum = 0
+            //if so, return that pair
+            //if > 0, decrement right (moving it one space to the left)
+            //else, increment left (moving it one space to the right)
 }
