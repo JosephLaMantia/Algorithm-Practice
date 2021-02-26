8987ecb05b64ffc39efd166275bd39b57cb0037e
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-02-26 14:24:44 -0500

    Completed sumZero

--- Udemy JS Algorithms and Data Structures/sumZero.js
+++ Udemy JS Algorithms and Data Structures/sumZero.js
@@ -4,10 +4,25 @@
 
 function sumZero (arr) {
     //create pointers for the left and rightmost elements of arr
+    let left = 0;
+    let right = arr.length - 1;
+
     //while the left is smaller than right
+    while(left < right){
         //create sum variable = left + right
+        let sum = arr[left] + arr[right];
+        console.log("left", arr[left], "right", arr[right], "sum", sum);
         //check if sum = 0
-            //if so, return that pair
-            //if > 0, decrement right (moving it one space to the left)
-            //else, increment left (moving it one space to the right)
+        if(sum === 0){  //if so, return that pair
+            return [arr[left], arr[right]];
+        } else if (sum > 0){    //if > 0, decrement right (moving it one space to the left)
+            right--;
+        } else {    //else, increment left (moving it one space to the right)
+            left++;
+        }
+    }    
 }
+
+let arr = [-5,-4,-3,1,3,10,9]
+
+console.log(sumZero(arr));
