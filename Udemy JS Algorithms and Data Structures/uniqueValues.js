64225daef0fb2c8aa31fc297a81b21833364cfe6
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-02-27 12:41:27 -0500

    Completed uniqueValues.js

---
+++ Udemy JS Algorithms and Data Structures/uniqueValues.js
@@ -0,0 +1,24 @@
+// from a sorted array of integers, count how many unique values there are
+
+function uniqueValues (arr){
+    //create two pointers
+    //point one to first index, then to second index.
+    let i = 0;
+    let j = 1;
+
+    while(j < arr.length){
+        //compare the two indexes
+        if(arr[j] === arr[i]){  //if same, move j forward
+            j++;
+        }
+        if(arr[j] !== arr[i]){  //if different move i forward and set the element at i to  the element at j
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    
+    return i;   //return i when j hits end of array
+}
+
+let arr = [-1, 0, 0, 1, 2, 3, 3, 4];
+console.log(uniqueValues(arr));
